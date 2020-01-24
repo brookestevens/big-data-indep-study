f46641271b62083d5dccfc1bf0d841652f4031b7
brookestevens <brst4163@colorado.edu>
2020-01-23 21:56:52 -0700

    adding node version

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const path = require('path');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 const express = require('express');
 const app = express();
